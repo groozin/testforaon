243e67b217b6bef4c1a62cf9fbeed4dd1465ba7e
Tomasz Mikos <tmikos@infusion.com>
2017-02-03 01:29:00 +0100

    Adds Infrastructure tests with first test.

---
+++ WebService/WebService.Infrastructure.Tests/InfrastructureTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace WebService.Infrastructure.Tests
+{
+    [TestFixture]
+    public class InfrastructureTests
+    {
+        [Test]
+        public void ItCanRunFirstTest()
+        {
+            Assert.AreEqual(2, 2);
+        }
+    }
+}
