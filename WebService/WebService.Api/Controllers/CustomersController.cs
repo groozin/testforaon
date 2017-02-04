ba21e44c2d3743727698fdcaa27b4839ab0dd8dd
Tomasz Mikos <tmikos@infusion.com>
2017-02-04 15:59:58 +0100

    Creates Web Api 2.2 project, enables CORS and introduces AutoFac DI.

---
+++ WebService/WebService.Api/Controllers/CustomersController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebService.Api.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class CustomersController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage GetCustomers()
+        {
+            var customers = new[] { new { Name = "Tomasz", Orders = "2" }, new { Name = "Tomasz", Orders = "2" }, new { Name = "Tomasz", Orders = "2" } };
+            return Request.CreateResponse(customers);
+        }
+    }
+}
---
+++ WebService/WebService.Api/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+
+namespace WebService.Api
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var builder = new ContainerBuilder();
+            var config = GlobalConfiguration.Configuration;
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
