16f56b6e85a3c34ea7c2fb91433c405667887d29
Tomasz Mikos <tmikos@infusion.com>
2017-02-06 22:54:10 +0100

    Adds support for multiple endpoints, depending on env version.

--- WebSite/src/config.js
+++ WebSite/src/config.js
@@ -1,3 +1,11 @@
-export const config = {
-    customersEndpointUrl : "http://localhost:19780/api/customers"
+let config = {
+    customersEndpointUrl: "http://localhost:19780/api/customers"
 }
+
+if (process.env.NODE_ENV === 'production') {
+     config = {
+        customersEndpointUrl: "http://testforaon.azurewebsites.net/api/customers"
+    }
+}
+
+export { config }
